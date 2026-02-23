97d4ce2b9d5c085f96d505e290e4de1fbd17984b
Muhammad Usman <zainusman1856@gmail.com>
2026-02-23 22:14:21 +0500

    Normalize line endings to LF

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   variable: "--font-inter",
